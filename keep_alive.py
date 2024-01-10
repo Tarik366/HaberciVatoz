f70201c1d50070dc5c5da1fe52fa4cd01ddf7ca6
Tarik366 <tariky113@gmail.com>
2024-01-10 06:43:22 +0300

    site combined with repl.it

--- keep_alive.py
+++ keep_alive.py
@@ -1,12 +1,21 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from threading import Thread
 import random
 
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
     return render_template('main.html')
+
+from i import ilist 
+x = ilist.keys()
+
+@app.route("/i")
+def images():
+    return render_template('images.html', x=x)
 
 from mongodb import get_adaklar
 
@@ -16,14 +25,12 @@ def adaklarSite():
     return render_template("adaklar.html", AdakList=AdakList)
 
 def run():
-    app.run(
-		host='0.0.0.0',
-		port=random.randint(2000,9000)
-	)
+    app.run(host='0.0.0.0', port=random.randint(2000, 9000))
+
 
 def keep_alive():
-	'''
+    '''
 	Creates and starts new thread that runs the function run.
 	'''
-	t = Thread(target=run)
-	t.start()
+    t = Thread(target=run)
+    t.start()

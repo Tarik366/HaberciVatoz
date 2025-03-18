4eade4587ab9802758921700385fcd24ade84776
Tarık <67359075+Tarik366@users.noreply.github.com>
2025-03-18 14:24:23 +0300

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -29,7 +29,7 @@ def adaklarSite():
     return render_template("adaklar.html", AdakList=AdakList)
 
 def run():
-    app.run(port=os.environ['fport'])
+    app.run(port=os.environ['PORT'])
 
 # Importing the library
 

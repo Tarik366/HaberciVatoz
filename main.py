9aec5337c89faeb567589a8685f0d7ae71adad21
Tarık <67359075+Tarik366@users.noreply.github.com>
2025-03-18 13:50:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,12 @@ from discord import * # type: ignore
 from discord.ext import commands, tasks
 import json
 import datetime as dt
-from keep_alive import keep_alive
 
 from dotenv.main import load_dotenv
 load_dotenv()
 
 # Discord bot settings
-cogs = ["cogs.buttons", "cogs.imageRenderer", "cogs.pp", "cogs.selectFromList", "cogs.randomCommands"]
+cogs = []
 
 class Client(commands.Bot):
     def __init__(self):
@@ -40,34 +39,13 @@ Bot = client
 
 from cogs.checkNews import News as News
 
-@Bot.command()
-async def sync(ctx):
-    print("sync command")
-    if ctx.author.id == 618214247742308361:
-        await client.tree.sync()
-        await ctx.send('Command tree synced.')
-    else:
-        await ctx.send('You must be the owner to use this command!')
-
 @client.event
 async def on_ready():
-    keep_alive()
     await News.msg1(client)
 
 @client.event
 async def on_guild_join(self, guild):
     await client.tree.sync()
-
-# Konuşma
-
-@client.event
-async def on_message(message):
-    if message.author == Bot.user:
-        return
-    if "alya" == message.content and message.author != Bot.user:
-        await message.channel.send("ALYA-SAMA!!!!!!")
-    else:
-        await Bot.process_commands(message)
 
 if discord.ext.commands.errors.CommandNotFound:
     print("")
@@ -77,7 +55,5 @@ def application():
         client.run(os.environ['discord_token'])
     except discord.errors.HTTPException:
         print("\n\n\nBLOCKED BY RATE LIMITS\nRESTARTING NOW\n\n\n")
-        os.system("python restarter.py")
-        os.system('kill 1')
 
 application()
